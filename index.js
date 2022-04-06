751c702fa41c0000921dd36886335a042fdade62
Daun Chung <daun@symless.com>
2022-04-06 17:58:22 +0100

    adding base path for .npmrc

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const core = require(`@actions/core`);
 
 const run = async () => {
   const token = core.getInput(`token`);
-
+  const basePath = core.getInput(`base-path`) || `.`;
   try {
-    await appendFile(`.npmrc`, `//npm.pkg.github.com/:_authToken=${token}`);
+    await appendFile(`${basePath}/.npmrc`, `//npm.pkg.github.com/:_authToken=${token}`);
   } catch (error) {
     core.setFailed(error.message);
   }
